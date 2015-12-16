146e85af3661311a1204e5b6a01198ca1ac29ed2
phoenix <pho.coderzhang@gmail.com>
2015-12-16 21:25:16 +0800

    init

---
+++ magpie-test-plumber-task/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to magpie-test-framework-app
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ magpie-test-plumber-task/src/com/jd/bdp/magpie/magpie_eggs_clj/magpie_test_plumber_task/bootstrap.clj
@@ -0,0 +1,10 @@
+(ns com.jd.bdp.magpie.magpie-eggs-clj.magpie-test-plumber-task.bootstrap)
+
+(use 'clojure.set)
+
+(def STATUS-INIT "init")
+(def STATUS-RUNNING "running")
+(def STATUS-FINISH "finish")
+(def STATUS-STOP "stop")
+
+(def SEPARATOR #"\*p\*")
