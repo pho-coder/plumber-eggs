1e3f5c50472894c9a5951a62aea3fa23d47881be
zengguangyao <zengguangyao@jd.com>
2015-12-25 15:04:32 +0800

    default-config

--- magpie-mysql2hadoop-plumber-task/src/com/jd/bdp/magpie/magpie_eggs_clj/magpie_mysql2hadoop_plumber_task/bootstrap.clj
+++ magpie-mysql2hadoop-plumber-task/src/com/jd/bdp/magpie/magpie_eggs_clj/magpie_mysql2hadoop_plumber_task/bootstrap.clj
@@ -12,3 +12,13 @@
 (def QUEEU-LENGTH 100)
 
 (def SEPARATOR #"\*p\*")
+
+(def BASE-CONF {:source "mysql"
+                :target "hadoop"
+                :sql "select * from a"
+                :jar "magpie-mysql2hadoop-plumber-task-0.0.1-SNAPSHOT-standalone.jar"
+                :klass "com.jd.bdp.magpie.magpie_eggs_clj.magpie_mysql2hadoop_plumber_task.core"
+                :group "default"
+                :type "memory"})
+
+; "plumber*p*albatross-test-0*p*test-job*p*task-0"
